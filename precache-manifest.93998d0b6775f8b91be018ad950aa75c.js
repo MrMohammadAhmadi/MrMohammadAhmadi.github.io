af6b86cfab3d9a47bb620d224b294e7ce9ad99f3
mohammadahmadi <47987499+MrMohammadAhmadi@users.noreply.github.com>
2020-10-14 12:59:29 +0330

    Updates

---
+++ precache-manifest.93998d0b6775f8b91be018ad950aa75c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e9686f96ccea826c764b41b742f5e07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de951c794fbb98b60a10",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8e120179933d61dbbb5d",
+    "url": "/static/css/main.a75b538c.chunk.css"
+  },
+  {
+    "revision": "de951c794fbb98b60a10",
+    "url": "/static/js/2.718380f1.chunk.js"
+  },
+  {
+    "revision": "d81c6c91afd2bb4bf8caccddf0472235",
+    "url": "/static/js/2.718380f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e120179933d61dbbb5d",
+    "url": "/static/js/main.9c05f224.chunk.js"
+  },
+  {
+    "revision": "334bc887324152744294",
+    "url": "/static/js/runtime-main.8ca5ca62.js"
+  },
+  {
+    "revision": "20f59ea72aea28ad422ff3bd635a0591",
+    "url": "/static/media/slider.20f59ea7.jpg"
+  }
+]);
